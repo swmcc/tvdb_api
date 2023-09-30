cd77de884eb62b755b4029b82d4720af851e2579
Stevie McCullough <stephen@skillfulgorilla.com>
2023-09-30 01:16:59 +0100

    ♻️  -Update TVDBApi to v0.0.4 with code improvements and optimizations
    - Bump version to 0.0.4
    - Introduce COMMON_PARAMS for shared API parameters
    - Refactor initialize method into smaller, focused methods
    - Rename request_endpoint to request for clarity
    - Integrate default_params into request

--- lib/tvdb_api/tvdb_api.rb
+++ lib/tvdb_api/tvdb_api.rb
@@ -6,16 +6,51 @@ require_relative 'tvdb_api_middleware'
 
 class TVDBApi
   BASE_URL = 'https://api4.thetvdb.com'
+  COMMON_PARAMS = { meta: 'translations', short: 'false' }.freeze
   ENDPOINTS = {
     search: '/v4/search',
-    movie: '/v4/movies/%{id}/extended',
+    movie:  '/v4/movies/%{id}/extended',
     series: '/v4/series/%{id}/extended',
     person: '/v4/people/%{id}/extended'
-  }
+  }.freeze
+
+  DEFAULT_PARAMS = {
+    movie: COMMON_PARAMS,
+    person: COMMON_PARAMS
+  }.freeze
 
   def initialize
-    Faraday::Request.register_middleware tvdb_auth: -> { TVDBAPIMiddleware }
+    register_middleware
+    setup_connection
+  end
 
+  def search(query)
+    request(:search, query: query)
+  end
+
+  def search_by_type(query, type)
+    request(:search, query: query, type: type)
+  end
+
+  def movie(id)
+    request(:movie, id: id)
+  end
+
+  def series(id)
+    request(:series, id: id)
+  end
+
+  def person(id)
+    request(:person, id: id)
+  end
+
+  private
+
+  def register_middleware
+    Faraday::Request.register_middleware tvdb_auth: -> { TVDBAPIMiddleware }
+  end
+
+  def setup_connection
     @conn = Faraday.new(url: BASE_URL) do |faraday|
       faraday.headers['Content-Type'] = 'application/json'
       faraday.request :url_encoded
@@ -24,31 +59,12 @@ class TVDBApi
     end
   end
 
-  def search(query)
-    request_endpoint(:search, query: query)
-  end
-
-  def search_by_type(query, type)
-    request_endpoint(:search, query: query, type: type)
-  end
-
-  def movie(id)
-    request_endpoint(:movie, id: id)
-  end
-
-  def series(id)
-    request_endpoint(:series, id: id)
-  end
-
-  def person(id)
-    request_endpoint(:person, id: id)
-  end
-
-  private
-
-  def request_endpoint(endpoint_key, params = {})
+  def request(endpoint_key, params = {})
     url = ENDPOINTS[endpoint_key]
     raise "Invalid endpoint key: #{endpoint_key}" unless url
+
+    default_params = DEFAULT_PARAMS[endpoint_key] || {}
+    params = params.merge(default_params)
 
     url = url % params if url.include?('%{')
 
@@ -60,3 +76,4 @@ class TVDBApi
     JSON.parse(response.body)
   end
 end
+
